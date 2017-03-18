860de44e83ee905d266be4a3d056c598a23a5449
xiaoxijin <xiaoxijin23@126.com>
2017-03-18 22:48:48 +0800

    测试提交功能

--- cleanup_docker.sh
+++ cleanup_docker.sh
@@ -2,6 +2,6 @@
 
 
 docker rm -v $(docker ps -a -q)
-
+#
 #docker rmi -f $(docker images -a | grep -v IMAGE | awk "{print \$3}")
 docker rmi -f $(docker images -a | grep "^<none>" | awk "{print \$3}")
